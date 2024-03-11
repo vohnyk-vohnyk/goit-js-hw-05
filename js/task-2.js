af3a57194b0a9a4391c286311def9aed55b42294
vohnyk-vohnyk <132850694+vohnyk-vohnyk@users.noreply.github.com>
2024-03-11 21:34:52 +0200

    done

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,60 @@
-function getShippingMessage(country, price, deliveryFee) {
+const getUsersWithFriend = (users, friendName) =>
+    users.filter(user => user.friends.includes(friendName));
 
-    const totalPrice = price + deliveryFee;
-    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-    return message;
-}
+// Перевірка роботи функції
+const allUsers = [
+    {
+        name: "Moore Hensley",
+        friends: ["Sharron Pace"]
+    },
+    {
+        name: "Sharlene Bush",
+        friends: ["Briana Decker", "Sharron Pace"]
+    },
+    {
+        name: "Ross Vazquez",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+    },
+    {
+        name: "Elma Head",
+        friends: ["Goldie Gentry", "Aisha Tran"]
+    },
+    {
+        name: "Carey Barr",
+        friends: ["Jordan Sampson", "Eddie Strong"]
+    },
+    {
+        name: "Blackburn Dotson",
+        friends: ["Jacklyn Lucas", "Linda Chapman"]
+    },
+    {
+        name: "Sheree Anthony",
+        friends: ["Goldie Gentry", "Briana Decker"]
+    }
+];
 
-console.log(getShippingMessage("USA", 1000, 50)); // "Shipping to USA will cost 1050 credits"
-console.log(getShippingMessage("Canada", 500, 30)); // "Shipping to Canada will cost 530 credits"
-console.log(getShippingMessage("UK", 1500, 70)); // "Shipping to UK will cost 1570 credits"
+console.log(getUsersWithFriend(allUsers, "Briana Decker"));
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
